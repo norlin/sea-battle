713f1e8ffc6fc9052032e9666442d6c5de740b03
norlin <alexey@norlin.guru>
2017-01-10 20:45:38 +0300

    Add player movement

--- src/client/js/player.js
+++ src/client/js/player.js
@@ -23,10 +23,77 @@ class ClientPlayer extends Element {
 		this.game.fillPlayerData(this, this.options);
 	}
 
+	setKeyboardTarget() {
+		let pos = this.pos();
+		let movement = false;
+
+		if (this._keyUP) {
+			movement = true;
+			pos.y -=  Number.MAX_VALUE;
+		}
+		if (this._keyDOWN) {
+			movement = true;
+			pos.y +=  Number.MAX_VALUE;
+		}
+		if (this._keyLEFT) {
+			movement = true;
+			pos.x -=  Number.MAX_VALUE;
+		}
+		if (this._keyRIGHT) {
+			movement = true;
+			pos.x +=  Number.MAX_VALUE;
+		}
+
+		if (movement) {
+			this.socket.emit('setTarget', {
+				x: pos.x,
+				y: pos.y
+			});
+		} else {
+			this.socket.emit('stop');
+		}
+	}
+
 	addListeners() {
 		this.socket.on('noEnergy', ()=>{
 			log.info('No energy!');
 		});
+
+		// UP
+		this.game.addKeyListener([Keys.UP, Keys.W], ()=>{
+			this._keyUP = true;
+			this.setKeyboardTarget();
+		}, ()=>{
+			this._keyUP = false;
+			this.setKeyboardTarget();
+		}, true);
+
+		// DOWN
+		this.game.addKeyListener([Keys.DOWN, Keys.S], ()=>{
+			this._keyDOWN = true;
+			this.setKeyboardTarget();
+		}, ()=>{
+			this._keyDOWN = false;
+			this.setKeyboardTarget();
+		}, true);
+
+		// LEFT
+		this.game.addKeyListener([Keys.LEFT, Keys.A], ()=>{
+			this._keyLEFT = true;
+			this.setKeyboardTarget();
+		}, ()=>{
+			this._keyLEFT = false;
+			this.setKeyboardTarget();
+		}, true);
+
+		// RIGHT
+		this.game.addKeyListener([Keys.RIGHT, Keys.D], ()=>{
+			this._keyRIGHT = true;
+			this.setKeyboardTarget();
+		}, ()=>{
+			this._keyRIGHT = false;
+			this.setKeyboardTarget();
+		}, true);
 
 		this.game.addMouseListener(false, (point)=>{
 			// on mouseup
--- src/server/player.js
+++ src/server/player.js
@@ -4,6 +4,9 @@ import Element from 'common/element';
 
 let log = new Log('Player');
 
+const SPEED_BASE = 6.25;
+const SPEED_RETURN = 20;
+
 class Player extends Element {
 	constructor(game, socket, options) {
 		super(game, options);
@@ -29,8 +32,23 @@ class Player extends Element {
 		let socket = this.socket;
 
 		socket.removeAllListeners('setTarget');
-		socket.on('click', (point)=>{
-			// let target = new Vector(point.x, point.y);
+		socket.on('setTarget', (point)=>{
+			this._speed = SPEED_BASE;
+
+			this.target = new Vector(point.x, point.y);
+		});
+
+		socket.removeAllListeners('stop');
+		socket.on('stop', (point)=>{
+			let id = this.fields[0];
+			let field = this.game.fields[id];
+
+			if (field) {
+				this._speed = SPEED_RETURN;
+				this.target = field.pos();
+			} else {
+				this.target = this._position.copy();
+			}
 		});
 	}
 
@@ -44,6 +62,8 @@ class Player extends Element {
 	initParams() {
 		super.initParams();
 
+		this._speed = SPEED_BASE;
+
 		let fields = [
 		];
 
@@ -52,7 +72,69 @@ class Player extends Element {
 		});
 	}
 
+	move() {
+		if (!this.target) {
+			return;
+		}
+
+		let compare = this.pos().sub(this.target);
+		if (Math.abs(compare.x) < this._speed && Math.abs(compare.y) < this._speed) {
+			this.stopMovement();
+
+			return;
+		}
+
+		let target = this.target.copy().sub(this._position);
+
+		if (!target.x && !target.y) {
+			return;
+		}
+
+		let dist = target.magnitude();
+		let delta = target.fromSelfAngle(this._speed);
+
+		let radius = this.radius;
+		let deltaDist = dist / (50 + radius);
+
+		if (dist < (50 + radius)) {
+			delta.multBy(deltaDist);
+		}
+
+		this._position.add(delta);
+
+		/*let width = this.game.config.width;
+		let height = this.game.config.height;
+
+		// jump behind the edges
+		if (this._position.x <= 0) {
+			this._position.x = width-1;
+			this.target.x = width + this.target.x;
+		} else if (this._position.x >= width) {
+			this._position.x = 1;
+			this.target.x = this.target.x - width;
+		}
+
+		if (this._position.y <= 0) {
+			this._position.y = height-1;
+			this.target.y = height + this.target.y;
+		} else if (this._position.y >= height) {
+			this._position.y = 1;
+			this.target.y = this.target.y - height;
+		}*/
+	}
+
+	stopMovement() {
+		if (this.target && this.target.isEqual(this._position)) {
+			this.target = null;
+			this._speed = SPEED_BASE;
+			return;
+		}
+
+		this.target = this._position.copy();
+	}
+
 	tick() {
+		this.move();
 		// TODO: count 5 sec to end turn
 	}
 
@@ -76,7 +158,7 @@ class Player extends Element {
 
 		let half = this.screen.copy().divBy(2);
 
-		let offset = 50;
+		let offset = 320;
 
 		return {
 			left: pos.x - half.x - offset,
